f5772a030f115eaf84491cbc5b4d65315aadc052
haziqam <103514359+haziqam@users.noreply.github.com>
2022-04-15 16:45:36 +0700

    Add files via upload

---
+++ F17.py
@@ -0,0 +1,13 @@
+#>>> exit
+
+import F16
+pilihan=str(input("Apakah Anda mau melakukan penyimpanan file yang telah diubah? (y/n) "))
+
+while not ((pilihan=='Y') or (pilihan=='y') or (pilihan=='N') or (pilihan=='n')):
+    pilihan=str(input("Apakah Anda mau melakukan penyimpanan file yang telah diubah? (y/n) "))
+
+if (pilihan=='Y') or (pilihan=='y'):
+    F16.save()
+
+#else:
+    #keluar dari aplikasi

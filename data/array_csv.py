ebf19d694b3eabb35789aa657f096e4c3be7824f
kailaeb <103516841+kailaeb@users.noreply.github.com>
2022-04-15 12:45:03 +0700

    Add files via upload

---
+++ data/array_csv.py
@@ -0,0 +1,25 @@
+game = [["id","nama","kategori","tahun_rilis","harga", "stok"],
+        ["GAME001", "BNMO - Play Along With Crypto","Adventure","2022",100000,1],
+        ["GAME002", "Dasar Pemrograman", "Coding", "2022", 0, 10],
+        ["GAME069", "Python Gemink", "Coding", "1991", 69000, 999],
+        ["GAME073", "Tubes", "Hardcore", "2022", 50000, 100],
+        ["GAME048", "Epep", "Battle", "2019", 10000, 2]]
+
+user = [["id", "username", "nama", "password", "role", "saldo"],
+        ["1","gege_akutami09", "Gege Akutami", "JJK0outnow", "Admin", 20000000],
+        ["2","sparkle__nobara", "Nobara Kugisaki", "buymecuteoutfits", "User", 5000000],
+        ["3", "itadori669", "Yuji Itadori", "Gojosensei_socool", "User", 7000000],
+        ["4", "Megumi", "Megumi Fushiguro", "iwannasleep", "User", 10000000],
+        ["5", "TheStrongest", "Gojo Satoru", "yowaimo", "Admin", 17000000]]
+
+kepemilikan = [["game_id", "user_id"],
+               ["GAME001", "ayam jago"],
+               ["GAME002", "bak u"],
+               ["GAME003", "camel"]]
+
+riwayat = [["game_id","nama","harga","user_id","tahun_beli"],
+           ["GAME003","Dota2",49900,"maxinsanity","2017"],
+           ["GAME019","CSGO",99000,"pejuangtembak","2013"],
+           ["GAME031", "PUBG", 249000, "dreamcatcher","2019"],
+           ["GAME175","Minecraft",129000,"kambingneon","2015"],
+           ["GAME231","Fortnite",59900,"kucinguwu","2020"]]

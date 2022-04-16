6535fee35192d8a26c4e43ab2fc8f327db0553d1
alkhairyfarisy <89649355+alkhairyfarisy@users.noreply.github.com>
2022-04-16 19:30:21 +0700

    Add files via upload

---
+++ F11.py
@@ -0,0 +1,14 @@
+from helpbase import *
+
+def search_game_at_store (id_game) :
+    id_game = input("Masukkan ID Game: ")
+    nama_game = input("Masukkan Nama Game: ")
+    harga_game = input("Masukkan Harga Game: ")
+    kategori_game = input("Masukkan Kategori Game: ")
+    tahun_rilis = input("Masukkan Tahun Rilis Game: ")
+
+    arr = filtergame(filtergame(filtergame(filtergame(filtergame(game_data,tahun_rilis,3),kategori_game,2),harga_game,4),nama_game,1),id_game,0)
+    for i in range (length(arr)) :
+        print(arr[i][0], " | ", arr[i][1], " | ", arr[i][4], " | ", arr[i][2], " | ", arr[i][3], " | ", arr[i][5])
+
+search_game_at_store("GAME002")

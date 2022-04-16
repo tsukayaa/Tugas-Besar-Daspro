949d87cd01298fec897ba79d7883ceb130f7f892
kailaeb <103516841+kailaeb@users.noreply.github.com>
2022-04-16 16:19:52 +0700

    Update perpustakaan_fungsi.py

--- perpustakaan_fungsi.py
+++ perpustakaan_fungsi.py
@@ -1,5 +1,5 @@
 
-def length(arrayName):
+def length(arrayName): #len(arr)
     panjang = 0
     for row in arrayName:
         panjang += 1
@@ -8,3 +8,14 @@ def length(arrayName):
 def saldovalid (saldo, balance) :
     if balance + saldo > 0 : return True
     else : return False
+
+def plus_arr (arr1,arr2): #arr.append
+    return arr1+arr2
+
+def remove_last (arr): #arr.pop
+    arr_baru = [0 for i in range (length(arr))]
+    for i in range (length(arr)-1):
+        arr_baru[i] = arr[i]
+    return arr_baru
+
+def 

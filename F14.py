15069a48b6335d72121ba4fc859ba7b731d905c6
haziqam <103514359+haziqam@users.noreply.github.com>
2022-04-16 16:36:41 +0700

    Add files via upload

---
+++ F14.py
@@ -0,0 +1,31 @@
+#admin
+print(""" ============ HELP ============
+1. register - Untuk melakukan registrasi user baru
+2. login - Untuk melakukan login ke dalam sistem
+3. tambah_game - Untuk menambah game yang dijual pada toko
+4. ubah_game - Untuk mengubah game pada toko game
+5. ubah_stok - Untuk mengubah stok game pada toko
+6. list_game_toko - Untuk melihat game yang ada pada toko
+7. search_game_at_store - Untuk mencari game di toko dari ID, nama game, harga, kategori, dan tahun rilis
+8. topup - Untuk menambahkan saldo kepada user
+9. help - Untuk melihat panduan penggunaan
+10. save - Untuk menyimpan perubahan
+11. exit - Untuk keluar
+ """)
+
+#user atau belum login
+print(""" ============ HELP ============
+1. login - Untuk melakukan login ke dalam sistem
+2. list_game_toko - Untuk melihat game yang ada pada toko
+3. buy_game - Untuk membeli game
+4. list_game - Untuk menampilkan daftar game yang dimiliki
+5. search_my_game - Untuk mencari game yang dimiliki dari ID dan tahun rilis
+6. search_game_at_store - Untuk mencari game di toko dari ID, nama game, harga, kategori, dan tahun rilis
+7. riwayat - Untuk melihat riwayat pembelian game
+8. help - Untuk melihat panduan penggunaan
+9. save - Untuk menyimpan perubahan
+10. exit - Untuk keluar
+""")
+
+
+

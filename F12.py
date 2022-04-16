22b92cb71453dfb941d57d1772adda56613282c5
alkhairyfarisy <89649355+alkhairyfarisy@users.noreply.github.com>
2022-04-16 16:42:22 +0700

    Update F12.py

--- F12.py
+++ F12.py
@@ -1,25 +1,29 @@
-from array_csv import user
-from perpustakaan_fungsi import length, saldovalid
+from perpustakaan_fungsi import length
 
-def topup (user) :
-    username = "Masukan username: "
-    saldo = "Masukan saldo: "
+def topup () :
+    global user_data
+    username = input("Masukan username: ")
+    saldo = input("Masukan saldo: ")
     isunamevalid = False
 
-    for i in range(1, length(user)) : 
-            if username == user[i][1] :
+    for i in range(0, length(user_data)) :
+            if username == user_data[i][1] :
                 isunamevalid = True
-                nama = user[i][2]
-                balance = int(user[i][5])
-                break
-
-    if isunamevalid :
-        if saldovalid(saldo, balance) : 
-            newbalance = balance + saldo
-            user[i][5] = str(newbalance)
-            print("Top up berhasil. Saldo {nama} bertambah menjadi {newbalance}.")
-        else : print("Masukan tidak valid")
+                nama = user_data[i][2]
+                balance = int(user_data[i][5])
+                urutanID = i
+    
+    if(not isunamevalid):
+        print(f"Username {username} tidak ditemukan.")
+    else:
+        if (int(user_data[urutanID][5]) + int(saldo)) < 0 :
+            print("Masukan tidak valid.")
+        else :
+            newbalance = int(saldo) + balance
+            user_data[urutanID][5] = str(newbalance)
+            print(f"Top up berhasil. Saldo {nama} bertambah menjadi {newbalance}.")
     return user
+topup()
 
 
 

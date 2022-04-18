d70a2d84e464d01a1a3f38dd10507c41d9dd254c
alkhairyfarisy <89649355+alkhairyfarisy@users.noreply.github.com>
2022-04-18 19:41:52 +0700

    Update F11.py

--- F11.py
+++ F11.py
@@ -6,8 +6,9 @@ def search_game_at_store () :
     harga_game = input("Masukkan Harga Game: ")
     kategori_game = input("Masukkan Kategori Game: ")
     tahun_rilis = input("Masukkan Tahun Rilis Game: ")
-
+    
     arr = filtergame(filtergame(filtergame(filtergame(filtergame(game_data,tahun_rilis,3),kategori_game,2),harga_game,4),nama_game,1),id_game,0)
+    print("Daftar game pada toko yang memenuhi kriteria: ")
     for i in range (length(arr)) :
         print(arr[i][0], " | ", arr[i][1], " | ", arr[i][4], " | ", arr[i][2], " | ", arr[i][3], " | ", arr[i][5])
 

0c1da94856e5992b672d7ef9c249a01422cd3c25
alkhairyfarisy <89649355+alkhairyfarisy@users.noreply.github.com>
2022-04-18 19:40:59 +0700

    Update F13.py

--- F13.py
+++ F13.py
@@ -5,6 +5,7 @@ def riwayat(userid) :
     for i in range (length(riwayat_data)) :
         if userid == riwayat_data[i][3] :
             cek = True
+            print("Daftar game: ")
             print(riwayat_data[i][0], " | ", riwayat_data[i][1], " | ", riwayat_data[i][2], " | ", riwayat_data[i][4])
     if cek == False :
         print("Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah beli_game untuk membeli.")

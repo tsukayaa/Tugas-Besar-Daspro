487c65f138035cdd1fd17c7746667b24a5bc7de1
alkhairyfarisy <89649355+alkhairyfarisy@users.noreply.github.com>
2022-04-18 18:47:02 +0700

    Add files via upload

---
+++ F13.py
@@ -0,0 +1,14 @@
+from helpbase import *
+
+def riwayat(userid) :
+    cek = False
+    for i in range (length(riwayat_data)) :
+        if userid == riwayat_data[i][3] :
+            cek = True
+            print(riwayat_data[i][0], " | ", riwayat_data[i][1], " | ", riwayat_data[i][2], " | ", riwayat_data[i][4])
+    if cek == False :
+        print("Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah beli_game untuk membeli.")
+
+# riwayat("0")
+# riwayat("1")
+# riwayat("3")
